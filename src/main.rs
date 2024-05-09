2902b1fb0b30221e70ed8344034f797abe8c4b14
0xcacti <0xcacti@gmail.com>
2024-05-09 19:17:23 -0400

    scaffold clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{crate_version, Parser, Subcommand};
+use std::{env, process};
+
+#[derive(Debug, Parser)]
+#[command(name="ring", version=crate_version!(), about="ping in rust", long_about = "rust implementation of the classic util ping", arg_required_else_help(true))]
+struct App {
+    /// The subcommand to run
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Ping,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = App::parse();
+
+    // handle commands
+    match &args.command {
+        Some(Commands::Ping) => {
+            println!("Pinging...")
+        }
+
+        None => {
+            eprintln!("No command provided");
+            process::exit(1);
+        }
+    }
 }
