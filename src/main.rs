fcec66cf002bb68bf5eca632a2458ec527eab79e
0xcacti <0xcacti@gmail.com>
2024-05-12 00:13:38 -0400

    add ipv6 and ipv4 parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{crate_version, Parser};
-use std::env;
+use std::{env, net::IpAddr};
 
 #[derive(Debug, Parser)]
 #[command(name="ring", version=crate_version!(), about="ping in rust", long_about = "rust implementation of the classic util ping", arg_required_else_help(true))]
@@ -12,6 +12,18 @@ struct App {
 async fn main() {
     let args = App::parse();
 
-    // handle commands
-    println!("Pinging {}", args.host);
+    match args.host.parse::<IpAddr>() {
+        Ok(ip) => match ip {
+            IpAddr::V4(ipv4) => {
+                println!("{} is a valid ip address", ipv4);
+            }
+            IpAddr::V6(ipv6) => {
+                println!("{} is a valid ip address", ipv6);
+            }
+        },
+        Err(_) => {
+            eprintln!("{} is not a valid ip address", args.host);
+            std::process::exit(1);
+        }
+    }
 }
