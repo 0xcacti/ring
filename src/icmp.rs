edb9da2099f64a385cf076f7cf59125e453d689c
0xcacti <0xcacti@gmail.com>
2024-06-11 15:09:55 -0400

    correctly compute checksum with test

--- src/icmp.rs
+++ src/icmp.rs
@@ -23,7 +23,7 @@ pub struct Header {
 pub struct ICMPHeader {
     pub msg_type: u8,
     pub code: u8,
-    pub icmp_checksum: u16,
+    pub checksum: u16,
     pub id: u16,
     pub seq_num: u16,
 }
@@ -38,7 +38,7 @@ pub struct Packet {
     pub icmp_payload: Option<ICMPPayload>,
 }
 
-fn get_machine_ipv4_ip() -> Option<Ipv4Addr> {
+fn get_machine_ipv4() -> Option<Ipv4Addr> {
     match get_if_addrs() {
         Ok(if_addrs) => {
             for if_addr in if_addrs {
@@ -48,6 +48,7 @@ fn get_machine_ipv4_ip() -> Option<Ipv4Addr> {
                     }
                 }
             }
+            return None;
         }
         Err(_) => panic!("Failed to get machine's IP address"), // TODO: should we panic here? or
                                                                 // set to localhost?
@@ -61,34 +62,78 @@ impl Packet {
             _ => panic!("Only IPv4 is supported"),
         };
 
+        let source_ip = get_machine_ipv4().unwrap().octets();
         let process_id = process::id() as u16;
+        let icmp_id = rand::random::<u16>();
+
+        let mut header = Header {
+            version: 4,
+            ihl: 5,
+            tos: 0,
+
+            // len(Header) + len(ICMPHeader) + 0 (no payload)
+            //     bytes: [ihl * 4(bytes)] + 2 * 4(bytes) + 32 * 4 + 0
+            length: 28,
+            id: process_id,
+            flags: 0,
+            fragment_offset: 0,
+            ttl: 64,
+            protocol: 1, // ICMP
+            checksum: 0,
+            source: source_ip,
+            destination: dest,
+        };
+        header.compute_checksum();
+
+        let mut icmp_header = ICMPHeader {
+            msg_type: 8, // echo request
+            code: 0,
+            checksum: 0,
+            id: icmp_id,
+            seq_num: 1,
+        };
+        icmp_header.compute_icmp_checksum();
 
         Packet {
-            header: Header {
-                version: 4,
-                ihl: 5,
-                tos: 0,
-
-                // len(Header) + len(ICMPHeader) + 0 (no payload)
-                //     bytes: [ihl * 4(bytes)] + 2 * 4(bytes) + 32 * 4 + 0
-                length: 28,
-                id: process_id,
-                flags: 0,
-                fragment_offset: 0,
-                ttl: 64,
-                protocol: 1, // ICMP
-                checksum: 0,
-                source: [127, 0, 0, 1],
-                destination: dest,
-            },
-            icmp_header: ICMPHeader {
-                msg_type: 8, // echo request
-                code: 0,
-                icmp_checksum: 0,
-                id: process_id,
-                seq_num: 0,
-            },
+            header,
+            icmp_header,
             icmp_payload: None,
         }
+    }
+}
+
+impl Header {
+    fn compute_checksum(&mut self) {}
+}
+
+impl ICMPHeader {
+    pub fn new_echo_request_header(id: u16, seq_num: u16) -> ICMPHeader {
+        ICMPHeader {
+            msg_type: 8,
+            code: 0,
+            checksum: 0,
+            id,
+            seq_num,
+        }
+    }
+    fn compute_icmp_checksum(&mut self) {
+        let msg_code_sum = (self.msg_type as u16) << 8 + self.code as u16;
+        let id_seq_sum = self.id + self.seq_num;
+        let subtotal = msg_code_sum + id_seq_sum;
+        let checksum = u16::MAX - subtotal;
+        self.checksum = checksum;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compute_icmp_checksum() {
+        let mut icmp_header = ICMPHeader::new_echo_request_header(0x1234, 0x001);
+        icmp_header.compute_icmp_checksum();
+        print!("{:X}", icmp_header.checksum);
+        assert_eq!(icmp_header.checksum, 0xE5CA);
     }
 }
