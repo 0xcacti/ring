4ee5f6fc0501927824419e6c9cdb2dc5cdd2ba54
0xcacti <0xcacti@gmail.com>
2024-06-26 16:48:37 -0400

    save

--- src/ip.rs
+++ src/ip.rs
@@ -3,41 +3,53 @@ use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 pub fn get_machine_ipv4(destination: Ipv4Addr) -> Option<Ipv4Addr> {
     if destination.is_loopback() {
-        return Some(Ipv4Addr::new(127, 0, 0, 1));
+        return Some(Ipv4Addr::LOCALHOST);
     }
-
-    match get_if_addrs() {
-        Ok(if_addrs) => {
-            for if_addr in if_addrs {
+    get_if_addrs().ok().and_then(|if_addrs| {
+        if_addrs
+            .into_iter()
+            .filter_map(|if_addr| {
                 if let IpAddr::V4(ipv4_addr) = if_addr.addr.ip() {
-                    if !ipv4_addr.is_loopback() {
-                        return Some(ipv4_addr);
+                    if !ipv4_addr.is_loopback() && !ipv4_addr.is_link_local() {
+                        Some(ipv4_addr)
+                    } else {
+                        None
                     }
+                } else {
+                    None
                 }
-            }
-            return None;
-        }
-        Err(_) => panic!("Failed to get machine's IP address"), // TODO: should we panic here? or
-    }
+            })
+            .next()
+    })
 }
 
-// TODO: learn how freaking IPv6 works
+// Helper function to check if an IPv6 address is link-local
+fn is_link_local(addr: &Ipv6Addr) -> bool {
+    addr.segments()[0] & 0xffc0 == 0xfe80
+}
+
 pub fn get_machine_ipv6(destination: Ipv6Addr) -> Option<Ipv6Addr> {
     if destination.is_loopback() {
-        return Some(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1));
+        return Some(Ipv6Addr::LOCALHOST);
     }
-
-    match get_if_addrs() {
-        Ok(if_addrs) => {
-            for if_addr in if_addrs {
+    get_if_addrs().ok().and_then(|if_addrs| {
+        if_addrs
+            .into_iter()
+            .filter_map(|if_addr| {
                 if let IpAddr::V6(ipv6_addr) = if_addr.addr.ip() {
-                    if !ipv6_addr.is_loopback() {
-                        return Some(ipv6_addr);
+                    if !ipv6_addr.is_loopback()
+                        && !is_link_local(&ipv6_addr)
+                        && ipv6_addr.is_global()
+                    {
+                        Some(ipv6_addr)
+                    } else {
+                        None
                     }
+                } else {
+                    None
                 }
-            }
-            return None;
-        }
-        Err(_) => panic!("Failed to get machine's IP address"), // TODO: should we panic here? or
-    }
+            })
+            .next()
+    })
 }
+
