8681f7484484475252e46a7a969efa3ff4ae7f0e
0xcacti <0xcacti@gmail.com>
2024-06-30 23:40:45 -0400

    save

--- src/main.rs
+++ src/main.rs
@@ -145,6 +145,94 @@ async fn ring_ipv4(
     );
 }
 
+// async fn ring_ipv6(
+//     source: IpAddr,
+//     destination: IpAddr,
+//     is_macos: bool,
+//     icmp_id: u16,
+//     args: CliArgs,
+// ) {
+//     let stats = Arc::new(Mutex::new(Stats::new()));
+//     let running = Arc::new(AtomicBool::new(true));
+//     let mut tasks = Vec::new();
+//
+//     let running_clone = running.clone();
+//     tokio::spawn(async move {
+//         signal::ctrl_c()
+//             .await
+//             .expect("Failed to listen for ctrl-c event");
+//         running_clone.store(false, Ordering::SeqCst);
+//         println!("\nInterrupted. Finishing current pings and collecting stats...");
+//     });
+//
+//     for i in 0..args.count.unwrap_or(u16::MAX) {
+//         let packet = if is_macos {
+//             icmp::IPV6Packet::new_echo_request(
+//                 true,
+//                 source,
+//                 destination,
+//                 icmp_id,
+//                 args.ttl,
+//                 args.include_payload,
+//                 i,
+//             )
+//         } else {
+//             icmp::IPV6Packet::new_echo_request(
+//                 false,
+//                 source,
+//                 destination,
+//                 icmp_id,
+//                 args.ttl,
+//                 args.include_payload,
+//                 i,
+//             )
+//         };
+//
+//         let stats = stats.clone();
+//         let destination = destination;
+//         let running = running.clone();
+//         let task = tokio::spawn(async move {
+//             let start = Instant::now();
+//             match socket::send_and_receive_ipv6_packet(
+//                 packet,
+//                 destination,
+//                 args.audio,
+//                 args.timeout,
+//                 &running,
+//             ) {
+//                 Ok(_) => {
+//                     let elapsed = start.elapsed();
+//                     let mut stats = stats.lock().await;
+//                     stats.update_success(elapsed);
+//                 }
+//                 Err(e) => {
+//                     eprintln!("{}", e.to_string());
+//                     let mut stats = stats.lock().await;
+//                     stats.update_failure();
+//                 }
+//             }
+//         });
+//         tasks.push(task);
+//         sleep(Duration::from_millis(args.interval)).await;
+//     }
+//
+//     for task in tasks {
+//         task.await.unwrap();
+//     }
+//
+//     let mut final_stats = stats.lock().await;
+//     let avg_success_time = final_stats.calculate_avg_success_time();
+//     println!(
+//         "Success: {} Failure: {} Avg Success Time: {}",
+//         final_stats.success,
+//         final_stats.failure,
+//         match avg_success_time {
+//             Some(duration) => duration.as_millis().to_string(),
+//             None => "N/A".to_string(),
+//         }
+//     );
+// }
+
 #[derive(Debug)]
 struct Stats {
     success: u32,
